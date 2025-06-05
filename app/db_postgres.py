503679ef777fa8c364bd400d8b9e5d0d4e72b6bc
tevander <tevander@cisco.com>
2025-06-05 14:06:05 -0700

    added gitignore and env files

--- app/db_postgres.py
+++ app/db_postgres.py
@@ -1,12 +1,17 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 def store_llm_response(deployment_id, response):
     try:
         conn = psycopg2.connect(
-            dbname="testdb",
-            user="teddyvw",
-            host="localhost",
-            port="5432",
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
         )
         cursor = conn.cursor()
 
